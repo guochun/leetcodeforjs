ae8855290334e26e4a1c831124087837cbe2a279
Purcy Guo <purcyguo@gmail.com>
2019-02-14 00:22:38 +0800

    添加 reverseWords 两个解决方案

--- code/String/ReverseWords.js
+++ code/String/ReverseWords.js
@@ -18,5 +18,18 @@ function reverseWords_test(s) {
   }).join(' ')
 }
 
+function reverseWords_test1(s) {
+  return s.split(/\s/g).map((v) => {
+    return v.split('').reverse().join('')
+  }).join(' ')
+}
+
+function reverseWords_test2(s) {
+  //判断空字符和所有字符都为空白字符
+  if(/^\s*$/.test(s)) return s
+  return s.match(/\S+/g).map((v) => {
+    return v.split('').reverse().join('')
+  }).join(' ')
+}
 
 export default reverseWords
