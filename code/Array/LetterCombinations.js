e01f0370c9c13dc4aba64c37b3e189e6e816385f
Purcy Guo <purcyguo@gmail.com>
2019-02-18 00:06:15 +0800

    完成LetterCombinations

--- code/Array/LetterCombinations.js
+++ code/Array/LetterCombinations.js
@@ -3,10 +3,31 @@
  * @return {string[]}
  */
 const letterCombinations = function(digits) {
-    const code = {
-      2: ['a','b','c'],
-      
+    let map = {
+        2:['a','b','c'],
+        3:['d','e','f'],
+        4:['g','h','i'],
+        5:['j','k','l'],
+        6: ['m','n','o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u','v'],
+        9: ['w','x','y','z']
     }
+    let combine = (arr_1, arr_2) => {
+        const arr = []
+        for (let i = 0, len = arr_1.length; i < len; i ++) {
+            for (let j = 0, len = arr_2.length; j < len; j ++) {
+                arr.push(`${arr_1[i]}${arr_2[j]}`)
+            }
+        }
+        return arr
+    }
+    return digits.split('').map(v => {
+        const res = map[v]
+        return res ? res : null
+    }).filter((v) => v != null ).reduce((res, p) =>{
+        return  res.length === 0 ? p : combine(res,p)
+    },[])
 };
 
 export default letterCombinations
---
+++ test/Array/LetterCombinations.test.js
@@ -0,0 +1,6 @@
+import letterCombinations from '../../code/Array/LetterCombinations'
+
+test('letterCombinations: ', () => {
+  expect(letterCombinations("23")).toEqual(["ad","ae","af","bd","be","bf","cd","ce","cf"])
+})
+
